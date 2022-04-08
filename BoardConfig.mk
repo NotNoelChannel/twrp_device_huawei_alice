584cd27568701526e0e82879a044bceafb3a4ee7
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-04-08 19:39:44 +0200

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/huawei/alice
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a15
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HIGH_OPTIMIZATION := true
+TARGET_USES_64_BIT_BINDER := true
+TARGET_BOARD_PLATFORM := hi6210sft
+BOARD_VENDOR_PLATFORM := hi6210sft
+
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := hi6210sft
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x07478000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := hisi_dma_print=0 vmalloc=384M maxcpus=8 coherent_pool=512K no_irq_affinity ate_enable=true androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x02988000
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+
+# Partitions
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 25165824
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11605639168
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+TW_EXCLUDE_SUPERSU := true
+
+TW_INPUT_BLACKLIST := "accelerometer"
+DEVICE_RESOLUTION := 720x1280
+
+TW_FBIOPAN := true
+RECOVERY_GRAPHICS_FORCE_USE_LINELENGTH := true
+BACKLIGHT_PATH := /sys/class/leds/lcd_backlight0/brightness
+TW_BRIGHTNESS_PATH := $(BACKLIGHT_PATH)
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
